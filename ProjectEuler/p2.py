156030b06312b382ba8d0deb12db90400d59e514
lebesgue <shuoxiangpub@gmail.com>
2012-11-15 22:49:23 -0700

    remove deleted files

--- ProjectEuler/p2.py
+++
@@ -1,8 +0,0 @@
-import sys
-s , x, x1, x2= [0, 0, 1, 0];
-while x <= 4000000:
-	x = x1 + x2;
-	if x <= 4000000 and x % 2 == 0:
-		s += x;
-	x2 = x1; x1 = x; 
-print (s);
--- ProjectEuler/p48.py
+++
@@ -1,5 +0,0 @@
-md = 10000000000;
-ans = 0;
-for i in range (1, 1001):
-	ans += i ** i;
-print (ans % md);
