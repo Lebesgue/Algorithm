58f8fe19020dc9519db1716c603d604a53a66997
Lebesgue <shuoxiangpub@gmail.com>
2013-02-01 16:06:56 -0700

    regular update

--- Codeforces/150C_107.cpp
+++ Codeforces/150C_107.cpp
@@ -53,10 +53,7 @@ qtype query (int u, int x, int y) {
 	qtype a = {-1e20, -1e20, -1e20}, b = {-1e20, -1e20, -1e20}, ret;
 	if (x <= mid) a = query (u<<1, x, y);
 	if (y > mid) b = query (u<<1|1, x, y);
-	ret.ma = max(a.ma, b.ma);
-	if (x <= mid && y > mid) {
-		ret.ma = max (ret.ma, a.mr + b.ml);
-	}
+	ret.ma = max (max (a.ma, b.ma), a.mr + b.ml);
 	ret.ml = a.ml;
 	if (x <= L[u] && y > mid)
 		ret.ml = max (ret.ml, sum[u<<1] + b.ml);
